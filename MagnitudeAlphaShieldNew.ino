def9fc2a037ad7521996880e56b1e10c68cb5325
Brent Luker <Brent Luker>
2016-03-31 22:46:15 -0700

    Help with Merge

--- MagnitudeAlphaShieldNew.ino
+++ MagnitudeAlphaShieldNew.ino
@@ -29,13 +29,8 @@ TempHumid th;
 Barometer b;
 CO2 c;
 
-<<<<<<< HEAD
 SoftwareSerial logger(LOGGER_TX, LOGGER_RX);
-
-#define NUM_SENSORS 5
-=======
 #define NUM_SENSORS 6
->>>>>>> CO2
 
 //This array is used when all the sensors are more or less doing the same thing.
 Sensor* sensors[NUM_SENSORS] = {&a, &m,&g, &th,&b, &c};
@@ -60,12 +55,13 @@ unsigned long lastLog = INITIAL_DELAY;
 void loop() {
   if( lastLog + READ_DELAY < millis()){
   lastLog = millis();
-<<<<<<< HEAD
     for(int8_t i=0; i<NUM_SENSORS; i++)
       sensors[i]->updateData();
     
     // Data logging
-    if(isSampling){
+    if(millis() > SAMPLE_TIME){
+      Serial.print( F("CO2: "));
+      Serial.print(c.co2);
       Serial.print( F("HIH-6130Temp: "));
       Serial.print( th.temp );
       Serial.print( F(",humidity: "));
@@ -103,6 +99,8 @@ void loop() {
       Serial.println();
     }
     else {
+      logger.print( F("CO2: "));
+      logger.print(c.co2);
       logger.print( F("HIH-6130Temp: "));
       logger.print( th.temp );
       logger.print( F(",humidity: "));
@@ -139,47 +137,6 @@ void loop() {
       logger.print( g.time);
       logger.print(F("\r\n"));
     }
-=======
-  
-  for(int8_t i=0; i<NUM_SENSORS; i++)
-    sensors[i]->updateData();
-
-  // Data logging
-  if( lastLog < millis()){
-    Serial.print( F("CO2: "));
-    Serial.print(c.co2);
-    Serial.print( F("th02Temp: "));
-    Serial.print( th.temp );
-    Serial.print( F(",humidity: "));
-    Serial.print( th.humidity );
-    Serial.print( F(",bmp180Temp: "));
-    Serial.print( b.temperature );
-    Serial.print( F(",pressure: "));
-    Serial.print( b.pressure );
-    Serial.print( F(",Altitude"));
-    Serial.print( b.altitude);
-    Serial.print( F("accel_x: " ) );
-    Serial.print( a.x );
-    Serial.print( F(",accel_y: " ) );
-    Serial.print( a.y );
-    Serial.print( F(",accel_z: " ) );
-    Serial.print( a.z );
-    Serial.print( F(",mag_x: " ) );
-    Serial.print( m.x );
-    Serial.print( F(",mag_y: " ) );
-    Serial.print( m.y );
-    Serial.print( F(",mag_z: " ) );
-    Serial.print( m.z);
-    Serial.print( F(",gps_lat: " ) );
-    Serial.print( g.lat,6);
-    Serial.print( F(",gps_lon: " ) );
-    Serial.print( g.lon,6);
-    Serial.print( F(",gps_alt: " ) );
-    Serial.print( g.alt);
-    Serial.print( F(",gps_speed(mps): " ) );
-    Serial.print( g.speed_mps);
-    Serial.println();
->>>>>>> CO2
     
     if(isSampling && millis() > SAMPLE_TIME){
       Serial.print(F("Sampling is done. Arduino will now log data to logger. You may unplug the serial cable."));
